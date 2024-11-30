b6d46a8e4f2a807c63a3dea952a9c4fb7f414591
opexylol <opexclaqz@gmail.com>
2024-11-30 16:21:13 +0530

    j

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="brewify",
-    version="0.1.1",  
+    version="0.1.2",  
     packages=find_packages(),
     author="OPEX",
     author_email="opexclaqz@gmail.com",
